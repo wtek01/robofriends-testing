af5fcb17ccad185fc1b714826b52b4e9a1e1d767
wtek01 <wtek01@gmail.com>
2020-07-16 18:52:26 +0200

    prettier 3

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,30 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class CounterButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
     };
   }
 
-  shouldComponentUpdate(nextProps,
-     nextState) {
+  shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
-                            return true;
+      return true;
     }
-    return false;}
+    return false;
+  }
 
   render() {
     return (
       <button
-        id='counter'
+        id="counter"
         color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
+        onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
+      >
         Count: {this.state.count}
       </button>
     );
   }
 }
 
-export default CounterButton
+export default CounterButton;
